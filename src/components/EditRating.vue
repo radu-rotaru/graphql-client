8335c8a06472e1bad036ea1ed88576b93edcc33c
unknown <rotrast@gmail.com>
2022-03-10 23:48:24 +0200

    Completed L6

---
+++ src/components/EditRating.vue
@@ -0,0 +1,46 @@
+<template>
+  <input
+    type="text"
+    v-model="rating"
+    :disabled="loading"
+    @keyup.enter="updateRating"
+    @keyup.esc="$emit('closeForm')"
+  />
+  <p v-if="loading"> Updating... </p>
+  <p v-if="error">{{ error }} </p> 
+</template>
+
+<script>
+  import { ref } from 'vue'
+  import UPDATE_BOOK_MUTATION from '../graphql/updateBook.mutation.gql'
+  import { useMutation } from '@vue/apollo-composable'
+
+  export default {
+    emits: ['closeForm'],
+    props: {
+      initialRating: {
+        type: Number,
+        required: true
+      },
+      bookId: {
+        type: String,
+        required: true
+      }
+    },
+    setup(props, { emit }) {
+      const rating = ref(props.initialRating)
+
+      const { mutate: updateRating, onDone, loading, error } = useMutation(UPDATE_BOOK_MUTATION, () => ({
+        variables: {
+          id: props.bookId,
+          rating: parseFloat(rating.value)
+        }
+      }))
+
+      onDone(() => {
+        emit('closeForm')
+      })
+      return {rating, updateRating, loading, error }
+    }
+  }
+</script>
